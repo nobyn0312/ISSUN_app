80470f9e7764a79347ac0e482e049b5ed298d2a6
nobyn0312 <nobyn0312@gmail.com>
2024-09-24 16:59:15 +0900

    UserInfoコンポーネント AuthcContextからのユーザー情報、名前などをヘッダーに表示させるためのコンポーネント

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,14 +1,41 @@
-import { auth } from '@/firebase'
-import Image from 'next/image'
-import React from 'react'
+import { auth } from "@/firebase";
+import Image from "next/image";
+import React, { useState } from "react";
+import SignOutButton from "./SignoutButton";
+
+import { useAuthContext } from "@/app/context/AuthContext";
 
 const UserInfo = () => {
-  return (
-    <div>
-      <Image src={auth.currentUser?.photoURL} width={100} height={100} alt="ユーザーアイコン"/>
-      <p>{auth.currentUser?.displayName}</p>
-    </div>
-  )
-}
+	const defaultPhotoURL = "/images/defaultUser.svg";
 
-export default UserInfo
+	const photoURL = auth.currentUser?.photoURL || defaultPhotoURL;
+	// const { username } = useAuthContext();
+
+	return (
+		<div>
+			{photoURL && (
+				<>
+					<div style={{ margin: "0 auto" }}>
+						<Image
+							src={photoURL}
+							width={24}
+							height={24}
+							alt='ユーザーアイコン'
+							className='rounded-full'
+							style={{ display: "block", margin: "0 auto" }}
+						/>
+						{/* <p style={{ fontSize: "10px", color: "black" }}>{username}</p> */}
+					</div>
+				</>
+			)}
+			{/* <p>{auth.currentUser?.displayName || "ゲスト"}</p>{" "} */}
+			{/* デフォルトの名前を指定 */}
+			{/* <p>{auth.currentUser?.displayName}</p> */}
+			{/* <dl>
+				<dd id='uid'></dd>
+			</dl> */}
+		</div>
+	);
+};
+
+export default UserInfo;
