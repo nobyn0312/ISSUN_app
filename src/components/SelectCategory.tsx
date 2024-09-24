1b47b03800faab980d000ef68ecddc1eba995a2d
nobyn0312 <nobyn0312@gmail.com>
2024-09-24 16:53:33 +0900

    SelectCategory 洋服のカテゴリ選択コンポーネント

---
+++ src/components/SelectCategory.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+interface SelectCategoryProps {
+	className?: string;
+}
+
+
+const SelectCategory: React.FC<SelectCategoryProps> = ({ className}) => {
+  return (
+		<>
+			<div className={`flex justify-end ${className}`}>
+				<select
+					name='category'
+					id='category-select'
+					className='text-black p-2 rounded-md border'
+				>
+					<option value=''>ALL</option>
+					<option value='outer'>アウター</option>
+					<option value='shirts'>Tシャツ・シャツ</option>
+					<option value='pants'>パンツ</option>
+				</select>
+			</div>
+		</>
+	);
+};
+
+export default SelectCategory;
