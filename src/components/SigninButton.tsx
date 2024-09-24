1c0b57c9c721bd90e6ccb0e0f45a1c7dbdb216df
nobyn0312 <nobyn0312@gmail.com>
2024-09-24 16:48:09 +0900

    signinButton サインインボタン googleでのログイン

--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -1,12 +1,12 @@
 "use client";  // クライアントコンポーネントとして明示
 
-
-import { auth, provider } from '../firebase';
+import { auth,provider } from '@/firebase';
 import { signInWithPopup } from 'firebase/auth';
 
 function SigninButton() {
+  
+
   const signInWithGoogle = () => {
-    // Firebaseの認証機能をクライアントサイドでのみ呼び出す
     if (typeof window !== 'undefined') {
       signInWithPopup(auth, provider).catch(error => {
         console.error("Google Sign-in Error:", error);
@@ -14,7 +14,11 @@ function SigninButton() {
     }
   };
 
-  return <button onClick={signInWithGoogle}>サインイン</button>;
+  return (
+    <>
+    <button onClick={signInWithGoogle}>Googleでログイン</button>
+    </>
+  );
 }
 
 export default SigninButton;
