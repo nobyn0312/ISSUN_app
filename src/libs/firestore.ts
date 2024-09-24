4271004aaea1ab59fa7af4604a08ce1238ec8cba
nobyn0312 <nobyn0312@gmail.com>
2024-09-24 16:45:21 +0900

    firebaseの初期化

---
+++ src/libs/firestore.ts
@@ -0,0 +1,18 @@
+// src/libs/firestore.ts
+
+import { firestore } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export const fetchItems = async () => {
+	try {
+		const querySnapshot = await getDocs(collection(firestore, "item"));
+		const items = querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data(),
+		}));
+		return items;
+	} catch (error) {
+		console.error("Error fetching items: ", error);
+		return [];
+	}
+};
