b56d4516cedbf9a33b3a0dee0aef4e623f193ab7
nobyn0312 <nobyn0312@gmail.com>
2024-09-24 16:57:57 +0900

    ページだけ用意したもの

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/Header'
+import Head from 'next/head'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <h2>マイページ</h2>
+
+      
+    </>
+  )
+}
+
+export default page
