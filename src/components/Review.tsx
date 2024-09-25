8efcacef54ba0ae7359ba42647a7a1354f79691d
nobyn0312 <nobyn0312@gmail.com>
2024-09-26 08:30:16 +0900

    reviewコンポーネント修正 firebaseからの取得 reviewの型指定

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,91 +1,145 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SecondaryButton } from "./Button";
 import Link from "next/link";
+import { firestore } from "@/firebase";
+import { collection, query, where, getDocs, Timestamp } from "firebase/firestore";
 
-// propsの型定義を追加
+
 type ReviewProps = {
 	itemId: string;
 };
 
+// review型
+type ReviewData = {
+	title: string;
+	comment: string;
+	username: string;
+	rate: number;
+	size: string;
+	createdAt:Timestamp;
+};
+
 const Review = ({ itemId }: ReviewProps) => {
 	console.log("アイテムID: ", itemId);
+
+		const [reviews, setReviews] = useState<ReviewData[]>([]);
+
 	// レビュータイトル
-	const [title, setTitle] = useState("");
-	// レート
-	const [rate, setRate] = useState("");
-	// コメント
-	const [comment, setComment] = useState("");
+	// const [title, setTitle] = useState("");
+	// const [rate, setRate] = useState("");
+	// const [comment, setComment] = useState("");
+
+
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
 		console.log("アイテムID: ", itemId);
 	};
 
-	return (
-		<section
-			style={{
-				background: "#ffffff",
-				color: "#333333",
-				borderRadius: "15px",
-				marginBottom: "24px",
-				padding: "16px",
-			}}
-		>
-			<div className='flex align-bottom items-end justify-between mb-4'>
-				<div style={{ width: "40%" }}>
-					<p style={{ fontSize: "16px", fontWeight: "bold" }}>minnano</p>
-					<h2 style={{ fontSize: "28px", fontWeight: "bold" }}>Review</h2>
+		const fetchReviews = async () => {
+			try {
+				const q = query(
+					collection(firestore, "review"),
+					where("itemId", "==", itemId)
+				);
 
-					<p>reviewer：32</p>
-				</div>
-				{/* <SecondaryButton style={{ width: "40%" }}>
-					<a href='/review/create/'>レビュー追加</a>
-				</SecondaryButton> */}
-				<Link href={`/review/create?itemId=${itemId}`}>レビューを書く</Link>
+				const querySnapshot = await getDocs(q);
+
+				const fetchedReviews: ReviewData[] = querySnapshot.docs.map((doc) => ({
+					title: doc.data().title,
+					comment: doc.data().comment,
+					username: doc.data().username,
+					rate: doc.data().rate,
+					size: doc.data().size,
+					createdAt: doc.data().createdAt,
+				}));
+				setReviews(fetchedReviews);
+			} catch (error) {
+				console.error("Error fetching reviews: ", error);
+			}
+
+
+		};
+		useEffect(() => {
+			fetchReviews();
+		}, [itemId]);
+
+
+return (
+	<section
+		style={{
+			background: "#ffffff",
+			color: "#333333",
+			borderRadius: "15px",
+			marginBottom: "24px",
+			padding: "16px",
+		}}
+	>
+		<div className='flex align-bottom items-end justify-between mb-4'>
+			<div style={{ width: "40%" }}>
+				<p style={{ fontSize: "16px", fontWeight: "bold" }}>minnano</p>
+				<h2 style={{ fontSize: "28px", fontWeight: "bold" }}>Review</h2>
+
+				<p>reviewer：{reviews.length}</p>
 			</div>
+			<Link href={`/review/create?itemId=${itemId}`}>レビューを書く</Link>
+		</div>
 
-			<div
-				style={{
-					padding: "16px",
-					background: "#E6E6E6",
-					borderRadius: "10px",
-				}}
-			>
-				<p
+		{reviews.map((review, index) => {
+			console.log(review.createdAt.toDate())
+			return (
+				<div
+					key={index}
 					style={{
-						fontWeight: "bold",
-						fontSize: "20px",
-						marginBottom: "8px",
+						padding: "16px",
+						background: "#E6E6E6",
+						borderRadius: "10px",
+						marginBottom: "24px",
 					}}
 				>
-					着丈ちょうどいい
-				</p>
-				<p style={{ fontWeight: "light", marginBottom: "8px" }}>2024/09/09</p>
+					<p
+						style={{
+							fontWeight: "bold",
+							fontSize: "20px",
+							marginBottom: "8px",
+						}}
+					>
+						{review.title}
+					</p>
+					<p style={{ fontWeight: "light", marginBottom: "8px" }}>
+						{review.createdAt.toDate().toString()}
+					</p>
 
-				<p style={{ color: "#FF5E2A", fontSize: "20px" }}>★★★★☆</p>
+					<p style={{ color: "#FF5E2A", fontSize: "20px" }}>
+						{/* rateを星の数に変換 */}
+						{"★".repeat(review.rate) + "☆".repeat(5 - review.rate)}
+					</p>
 
-				<p style={{ fontSize: "20px", fontWeight: "bold" }}>SIZE：S</p>
-				<div
-					style={{
-						border: "1px solid #333",
-						width: "100%",
-						marginBottom: "8px",
-					}}
-				></div>
-				<p style={{ lineHeight: "1.75", marginBottom: "60px" }}>
-					白のMサイズを購入 大きめで着るのが好きなので袖も長めで
-					ちょうどいいです 着丈も長めですがいつもインして着ています
-					洗濯してもシワになりづらく、お気に入りです
-				</p>
+					<p style={{ fontSize: "20px", fontWeight: "bold" }}>
+						SIZE：{review.size}
+					</p>
+					<div
+						style={{
+							border: "1px solid #333",
+							width: "100%",
+							marginBottom: "8px",
+						}}
+					></div>
+					<p style={{ lineHeight: "1.75", marginBottom: "60px" }}>
+						{review.comment}
+					</p>
 
-				<p>
-					<small>一寸先は闇/男性/160cm/やや細身</small>
-				</p>
-			</div>
-		</section>
-	);
+					<p>
+						<small>{review.username}</small>
+					</p>
+				</div>
+			);
+
+})}
+	</section>
+);
 };
 
 export default Review;
