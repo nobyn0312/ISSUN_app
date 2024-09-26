4b976e2c029c08b6c4f2c82c62808198e73ec798
nobyn0312 <nobyn0312@gmail.com>
2024-09-26 09:39:33 +0900

    Reviewコンポーネント修正

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -4,8 +4,14 @@ import React, { useEffect, useState } from "react";
 import { SecondaryButton } from "./Button";
 import Link from "next/link";
 import { firestore } from "@/firebase";
-import { collection, query, where, getDocs, Timestamp } from "firebase/firestore";
-
+import {
+	collection,
+	query,
+	where,
+	getDocs,
+	Timestamp,
+} from "firebase/firestore";
+import { Container } from "./Container";
 
 type ReviewProps = {
 	itemId: string;
@@ -18,128 +24,124 @@ type ReviewData = {
 	username: string;
 	rate: number;
 	size: string;
-	createdAt:Timestamp;
+	createdAt: Timestamp;
 };
 
 const Review = ({ itemId }: ReviewProps) => {
 	console.log("アイテムID: ", itemId);
 
-		const [reviews, setReviews] = useState<ReviewData[]>([]);
+	const [reviews, setReviews] = useState<ReviewData[]>([]);
 
 	// レビュータイトル
 	// const [title, setTitle] = useState("");
 	// const [rate, setRate] = useState("");
 	// const [comment, setComment] = useState("");
 
-
-
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
 		console.log("アイテムID: ", itemId);
 	};
 
-		const fetchReviews = async () => {
-			try {
-				const q = query(
-					collection(firestore, "review"),
-					where("itemId", "==", itemId)
-				);
-
-				const querySnapshot = await getDocs(q);
-
-				const fetchedReviews: ReviewData[] = querySnapshot.docs.map((doc) => ({
-					title: doc.data().title,
-					comment: doc.data().comment,
-					username: doc.data().username,
-					rate: doc.data().rate,
-					size: doc.data().size,
-					createdAt: doc.data().createdAt,
-				}));
-				setReviews(fetchedReviews);
-			} catch (error) {
-				console.error("Error fetching reviews: ", error);
-			}
-
-
-		};
-		useEffect(() => {
-			fetchReviews();
-		}, [itemId]);
-
-
-return (
-	<section
-		style={{
-			background: "#ffffff",
-			color: "#333333",
-			borderRadius: "15px",
-			marginBottom: "24px",
-			padding: "16px",
-		}}
-	>
-		<div className='flex align-bottom items-end justify-between mb-4'>
-			<div style={{ width: "40%" }}>
-				<p style={{ fontSize: "16px", fontWeight: "bold" }}>minnano</p>
-				<h2 style={{ fontSize: "28px", fontWeight: "bold" }}>Review</h2>
-
-				<p>reviewer：{reviews.length}</p>
-			</div>
-			<Link href={`/review/create?itemId=${itemId}`}>レビューを書く</Link>
-		</div>
-
-		{reviews.map((review, index) => {
-			console.log(review.createdAt.toDate())
-			return (
-				<div
-					key={index}
-					style={{
-						padding: "16px",
-						background: "#E6E6E6",
-						borderRadius: "10px",
-						marginBottom: "24px",
-					}}
-				>
-					<p
-						style={{
-							fontWeight: "bold",
-							fontSize: "20px",
-							marginBottom: "8px",
-						}}
-					>
-						{review.title}
-					</p>
-					<p style={{ fontWeight: "light", marginBottom: "8px" }}>
-						{review.createdAt.toDate().toString()}
-					</p>
-
-					<p style={{ color: "#FF5E2A", fontSize: "20px" }}>
-						{/* rateを星の数に変換 */}
-						{"★".repeat(review.rate) + "☆".repeat(5 - review.rate)}
-					</p>
-
-					<p style={{ fontSize: "20px", fontWeight: "bold" }}>
-						SIZE：{review.size}
-					</p>
-					<div
-						style={{
-							border: "1px solid #333",
-							width: "100%",
-							marginBottom: "8px",
-						}}
-					></div>
-					<p style={{ lineHeight: "1.75", marginBottom: "60px" }}>
-						{review.comment}
-					</p>
-
-					<p>
-						<small>{review.username}</small>
-					</p>
-				</div>
+	const fetchReviews = async () => {
+		try {
+			const q = query(
+				collection(firestore, "review"),
+				where("itemId", "==", itemId)
 			);
 
-})}
-	</section>
-);
+			const querySnapshot = await getDocs(q);
+
+			const fetchedReviews: ReviewData[] = querySnapshot.docs.map((doc) => ({
+				title: doc.data().title,
+				comment: doc.data().comment,
+				username: doc.data().username,
+				rate: doc.data().rate,
+				size: doc.data().size,
+				createdAt: doc.data().createdAt,
+			}));
+			setReviews(fetchedReviews);
+		} catch (error) {
+			console.error("Error fetching reviews: ", error);
+		}
+	};
+	useEffect(() => {
+		fetchReviews();
+	}, [itemId]);
+
+	return (
+		<>
+				<section
+					style={{
+						background: "#ffffff",
+						color: "#333333",
+						borderRadius: "15px",
+						marginBottom: "24px",
+						padding: "16px",
+					}}
+				>
+					<div className='flex align-bottom items-end justify-between mb-4'>
+						<div style={{ width: "40%" }}>
+							<p style={{ fontSize: "16px", fontWeight: "bold" }}>minnano</p>
+							<h2 style={{ fontSize: "28px", fontWeight: "bold" }}>Review</h2>
+
+							<p>reviewer：{reviews.length}</p>
+						</div>
+						<Link href={`/review/create?itemId=${itemId}`}>レビューを書く</Link>
+					</div>
+
+					{reviews.map((review, index) => {
+						console.log(review.createdAt.toDate());
+						return (
+							<div
+								key={index}
+								style={{
+									padding: "16px",
+									background: "#E6E6E6",
+									borderRadius: "10px",
+									marginBottom: "24px",
+								}}
+							>
+								<p
+									style={{
+										fontWeight: "bold",
+										fontSize: "20px",
+										marginBottom: "8px",
+									}}
+								>
+									{review.title}
+								</p>
+								<p style={{ fontWeight: "light", marginBottom: "8px" }}>
+									{review.createdAt.toDate().toString()}
+								</p>
+
+								<p style={{ color: "#FF5E2A", fontSize: "20px" }}>
+									{/* rateを星の数に変換 */}
+									{"★".repeat(review.rate) + "☆".repeat(5 - review.rate)}
+								</p>
+
+								<p style={{ fontSize: "20px", fontWeight: "bold" }}>
+									SIZE：{review.size}
+								</p>
+								<div
+									style={{
+										border: "1px solid #333",
+										width: "100%",
+										marginBottom: "8px",
+									}}
+								></div>
+								<p style={{ lineHeight: "1.75", marginBottom: "60px" }}>
+									{review.comment}
+								</p>
+
+								<p>
+									<small>{review.username}</small>
+								</p>
+							</div>
+						);
+					})}
+				</section>
+		</>
+	);
 };
 
 export default Review;
