b732da58a0d74a8077768accf0bd0e36db738ac3
nobyn0312 <nobyn0312@gmail.com>
2024-09-27 14:44:23 +0900

    ボタンコンポーネント 位置調整

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,21 @@ type ButtonProps = {
 	className?: string; // 追加のクラスを受け取る
 	type?: "button" | "submit" | "reset"; // typeプロパティを追加
 	style?: React.CSSProperties; // styleをpropsとして追加
+
+	position?: "left" | "center" | "right"; // positionオプショナル追加
+};
+
+const getPositionStyles = (position?: "left" | "center" | "right") => {
+	switch (position) {
+		case "left":
+			return { justifyContent: "flex-start" };
+		case "center":
+			return { justifyContent: "center" };
+		case "right":
+			return { justifyContent: "flex-end" };
+		default:
+			return { justifyContent: "flex-start" }; // デフォルトは左揃え
+	}
 };
 
 export const PrimaryButton: React.FC<ButtonProps> = ({
@@ -14,20 +29,30 @@ export const PrimaryButton: React.FC<ButtonProps> = ({
 	className,
 	style,
 	type = "button",
+
+	position = "center",
 }) => {
 	return (
-		<button
-			onClick={onClick}
-			className={`text-white font-bold rounded-full block w-full text-center p-3  ${className}`}
+		<div
 			style={{
-				background: "#FF5E2A",
-				maxWidth: "320px",
-				height: "50px",
-				...style,
+				display: "flex",
+				width: "100%",
+				...getPositionStyles(position), // 位置スタイルを適用
 			}}
 		>
-			{children}
-		</button>
+			<button
+				onClick={onClick}
+				className={`text-white font-bold rounded-full block w-full text-center p-3  ${className}`}
+				style={{
+					background: "#FF5E2A",
+					maxWidth: "320px",
+					height: "50px",
+					...style,
+				}}
+			>
+				{children}
+			</button>
+		</div>
 	);
 };
 
@@ -37,20 +62,30 @@ export const SecondaryButton: React.FC<ButtonProps> = ({
 	className,
 	style,
 	type = "button",
+
+	position = "center",
 }) => {
 	return (
-		<button
-			onClick={onClick}
-			className={`font-bold rounded-full block w-full text-center p-3 bg-white ${className}`}
+		<div
 			style={{
-				color: "#FF5E2A",
-				border: "#FF5E2A solid 2px",
-				maxWidth: "320px",
-				height: "50px",
-				...style,
+				display: "flex",
+				width: "100%",
+				...getPositionStyles(position), // 位置スタイルを適用
 			}}
 		>
-			{children}
-		</button>
+			<button
+				onClick={onClick}
+				className={`font-bold rounded-full block w-full text-center p-3 bg-white ${className}`}
+				style={{
+					color: "#FF5E2A",
+					border: "#FF5E2A solid 2px",
+					maxWidth: "320px",
+					height: "50px",
+					...style,
+				}}
+			>
+				{children}
+			</button>
+		</div>
 	);
 };
