6bf648de5d2d5ddf22d22135fd347847c42f3baf
nobyn0312 <nobyn0312@gmail.com>
2024-10-02 17:40:53 +0900

    signInButton googleサインインの調整

--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -1,24 +1,35 @@
-"use client";  // クライアントコンポーネントとして明示
+"use client"; // クライアントコンポーネントとして明示
 
-import { auth,provider } from '@/firebase';
-import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from "@/firebase";
+import { signInWithPopup } from "firebase/auth";
+
+import Image from "next/image";
 
 function SigninButton() {
-  
+	const signInWithGoogle = () => {
+		if (typeof window !== "undefined") {
+			signInWithPopup(auth, provider).catch((error) => {
+				console.error("Google Sign-in Error:", error);
+			});
+		}
+	};
 
-  const signInWithGoogle = () => {
-    if (typeof window !== 'undefined') {
-      signInWithPopup(auth, provider).catch(error => {
-        console.error("Google Sign-in Error:", error);
-      });
-    }
-  };
-
-  return (
-    <>
-    <button onClick={signInWithGoogle}>Googleでログイン</button>
-    </>
-  );
+	return (
+		<>
+			<button onClick={signInWithGoogle} style={{ color: "#ff5e2a" }}>
+				<span className='flex'>
+          <Image
+            style={{marginRight:"8px"}}
+						src='/images/google_icon.svg'
+						width={20}
+						height={20}
+						alt='googleアイコン'
+					/>
+					Googleログイン
+				</span>
+			</button>
+		</>
+	);
 }
 
 export default SigninButton;
