22c1cc3d8e9e2549a6e57789b2925e9f8df4f507
nobyn0312 <nobyn0312@gmail.com>
2024-10-04 11:11:16 +0900

    mockdatajsonでの取得テスト

--- src/libs/fetchItems.ts
+++ src/libs/fetchItems.ts
@@ -1,11 +1,12 @@
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "@/firebase";
+import mockData from "@/mockdata.json"; // ルートがsrcの場合 "@/mockdata.json"でアクセス
 
 // Item型を定義
 export interface Item {
 	id: string;
 	category: string;
-	createdAt: string;
+	createdAt?: string;
 	detail: string;
 	imageUrl: string;
 	name: string;
@@ -14,13 +15,25 @@ export interface Item {
 	url?: string | undefined;
 }
 
-export const fetchItems = async (): Promise<Item[]> => {
-	const itemsCollection = collection(firestore, "item");
-	const snapshot = await getDocs(itemsCollection);
+// export const fetchItems = async (): Promise<Item[]> => {
+// 	const itemsCollection = collection(firestore, "item");
+// 	const snapshot = await getDocs(itemsCollection);
 
-	const itemsList: Item[] = snapshot.docChanges().map((change) => ({
-		id: change.doc.id,
-		...(change.doc.data() as Omit<Item, "id">),
+// 	const itemsList: Item[] = snapshot.docChanges().map((change) => ({
+// 		id: change.doc.id,
+// 		...(change.doc.data() as Omit<Item, "id">),
+// 	}));
+
+// 	return itemsList;
+// };
+
+
+
+export const fetchItems = async (): Promise<Item[]> => {
+	// mockdata.jsonのデータをそのまま返す
+	const itemsList: Item[] = mockData.map((item) => ({
+		...item,
+		price: Number(item.price), // priceを数値に変換
 	}));
 
 	return itemsList;
