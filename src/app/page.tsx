78d530361055ed1f00526775fbc9073cc2bd16ff
nobyn0312 <nobyn0312@gmail.com>
2024-10-20 21:03:32 +0900

    ルートページの内容を削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,10 @@
-"use client";
+import React from 'react'
 
-import SigninButton from "@/components/SigninButton";
-import { auth } from "@/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import UserInfo from "@/components/UserInfo";
-import SignOutButton from "@/components/SignoutButton";
-import Image from "next/image";
-import { useState } from "react";
-
-const handleRegister = async (email: string, password: string) => {
-	try {
-		const userCredential = await createUserWithEmailAndPassword(
-			auth,
-			email,
-			password
-		);
-		const user = userCredential.user;
-	} catch (error) {
-		alert("ユーザー登録に失敗");
-	}
-};
-
-export default function Home() {
-	const [user] = useAuthState(auth);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-
-	const onSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
-		await handleRegister(email, password);
-	};
-
+const page = () => {
 	return (
 		<>
-			<main style={{ padding: "24px" }}>
-				<section>
-					<div>
-						<h1 className=''>
-							<Image
-								src='/images/topLogo.webp'
-								width={480}
-								height={480}
-								alt='BOXロゴ'
-							/>
-						</h1>
-					</div>
-					<div>
-						{user ? (
-							<>
-								<UserInfo />
-								<SignOutButton />
-							</>
-						) : (
-							<>
-								<p>ログイン</p>
-								<form onSubmit={onSubmit}>
-									<div>
-										<label htmlFor='email'>Email:</label>
-										<input
-											type='email'
-											id='email'
-											value={email}
-											onChange={(e) => setEmail(e.target.value)}
-											required
-										/>
-									</div>
-									<div>
-										<label htmlFor='password'>Password:</label>
-										<input
-											type='password'
-											id='password'
-											value={password}
-											onChange={(e) => setPassword(e.target.value)}
-											required
-											autoComplete='current-password'
-										/>
-									</div>
-									<button type='submit'>新規登録</button>
-								</form>
-
-								<p>Google認証</p>
-								<SigninButton />
-							</>
-						)}
-					</div>
-				</section>
-			</main>
 		</>
-	);
+	)
 }
+
+export default page
